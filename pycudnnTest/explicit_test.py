6553c32c96d6046b45c1c7e1badb4c203b3dca4a
Maximilien Breughe <mbreughe@nvidia.com>
2023-07-11 08:51:05 -0700

    Conv test and failing gemm explicit tests

---
+++ pycudnnTest/explicit_test.py
@@ -0,0 +1,151 @@
+import pycudnn
+import torch
+import torch.nn.functional as F
+
+import pytest
+
+def test_hello_world():
+    assert True == True
+
+class MBGraphModule:
+    def __init__(self, cudnn_graph):
+        self.cudnn_graph = cudnn_graph
+        self.variant_pack = {}
+        self.debug = True
+
+    def add_gemm(self, x, w):
+        b1, m1, k1 = x.size()
+        b2, k2, n1 = w.size()
+
+        assert (b1 == b2 or b2 == 1)
+        assert (k1 == k2)
+
+        self.problem_size = [b1, m1, n1]
+
+        image = self.cudnn_graph.tensor(name = "image", dim = x.size(), stride = x.stride(), data_type = convert_to_cudnn_type(x.dtype))
+        weight = self.cudnn_graph.tensor(name = "weight", dim = w.size(), stride = w.stride(), data_type = convert_to_cudnn_type(w.dtype))
+
+        self.variant_pack[image] = x.data_ptr()
+        self.variant_pack[weight] = w.data_ptr()
+
+        gemm_output = self.cudnn_graph.matmul(name = "mb_matmul", image = image, weight = weight, compute_data_type = pycudnn.data_type.FLOAT)
+        
+        # DEBUG
+        if self.debug:
+            gemm_output.set_stride([m1*n1, 1, m1])
+        else:
+            gemm_output.set_stride([m1*n1, n1, 1])
+
+        self.last_output = gemm_output
+        self.gemm_output = gemm_output
+        return gemm_output
+
+    def add_relu(self):
+        self.last_output.set_is_virtual(True)
+
+        activation_output = self.cudnn_graph.relu(name = "relu", input = self.last_output, compute_data_type = pycudnn.data_type.FLOAT)
+        print(activation_output)
+        b, m, n = self.problem_size
+        
+        # DEBUG
+        if self.debug:
+            activation_output.set_stride([m*n, 1, m])
+        else:
+            activation_output.set_stride([m*n, n, 1])
+
+        self.last_output = activation_output
+        return activation_output
+
+def convert_to_cudnn_type(torch_type):
+    if torch_type == torch.float16:
+        return pycudnn.data_type.HALF
+    elif torch_type == torch.float32:
+        return pycudnn.data_type.FLOAT
+    else:
+        raise ValueError("Unsupported tensor data type.")
+
+    return
+
+# Make backend testing optional and allow for "checkpoint"
+@pytest.mark.parametrize("in_dim, expected_gemm_out_dim", [([16, 32, 64, 128], [16,32,64,])])
+def test_gemm_relu_explicit(in_dim, expected_gemm_out_dim):
+    # Can put this in fixture:
+    cudnn_graph = pycudnn.pygraph("cudnn_graph", io_data_type = pycudnn.data_type.HALF, intermediate_data_type = pycudnn.data_type.FLOAT, compute_data_type = pycudnn.data_type.FLOAT)
+    mb_graph = MBGraphModule(cudnn_graph)
+
+    B, M, N, K = in_dim
+    x = torch.randn(B, M, K, device="cuda", dtype=torch.float16)
+    w = torch.randn(B, K, N, device="cuda", dtype=torch.float16)
+
+    gemm_output = mb_graph.add_gemm(x,w)
+    Y = mb_graph.add_relu()
+    Y.set_output(True)
+
+    output = torch.zeros(mb_graph.problem_size, dtype=torch.float, device='cuda')
+
+    mb_graph.variant_pack[mb_graph.last_output] = output.data_ptr()
+
+    print("Output strides: ", mb_graph.last_output.get_stride())
+
+    print(mb_graph.cudnn_graph)
+
+    mb_graph.cudnn_graph.build()
+
+
+
+    # Check expected shapes
+    assert expected_gemm_out_dim == gemm_output.get_dim()
+
+    # Compare output with a reference implementation
+    mb_graph.cudnn_graph.execute(mb_graph.variant_pack)
+    pyt_out = torch.bmm(x, w)
+    pyt_out = F.relu(pyt_out)
+    torch.testing.assert_close(output, pyt_out)
+
+class CSBR(torch.nn.Module):
+    def forward(self, x, w, b = None, padding = [1,1], stride = [1,1], dilation = [1,1]):
+        if b is not None:
+            b = b.reshape(-1) # Conv2d needs a 1D tensor
+        conv_output = torch.nn.functional.conv2d(x, w, bias = b, padding=padding, stride=stride, dilation=dilation)
+        return torch.nn.functional.relu(conv_output)
+
+def test_conv_relu():
+    # Reference code
+    X_gpu = torch.randn(20, 40, 30, 40, requires_grad=False, device="cuda", dtype=torch.float16).to(memory_format=torch.channels_last)
+    W_gpu = torch.randn(54, 40, 3, 4, requires_grad=False, device="cuda", dtype=torch.float16).to(memory_format=torch.channels_last)
+    padding = [0,1]
+    stride = [2,3]
+    dilation = [1,1]
+
+    model = CSBR().eval().to("cuda").to(torch.float16)
+    Y_expected = model(X_gpu, W_gpu, padding = padding, stride = stride, dilation = dilation)
+
+    # Cudnn code
+    graph = pycudnn.pygraph("conv-bias", io_data_type = pycudnn.data_type.HALF, intermediate_data_type = pycudnn.data_type.FLOAT, compute_data_type = pycudnn.data_type.FLOAT)
+
+    X = graph.tensor(name = "X", dim = X_gpu.size(), stride = X_gpu.stride(), data_type = convert_to_cudnn_type(X_gpu.dtype))
+    W = graph.tensor(name = "W", dim = W_gpu.size(), stride = W_gpu.stride(), data_type = convert_to_cudnn_type(W_gpu.dtype))
+    
+    conv_output = graph.conv(name = "conv", image = X, weight = W, padding = padding, stride = stride, dilation = dilation)
+
+    Y = graph.relu(name = "relu", input = conv_output)
+    Y.set_output(True)
+    
+    graph.build()
+
+    workspace = torch.empty(graph.get_workspace_size(), device="cuda", dtype=torch.uint8)
+
+    Y_actual = torch.zeros(*conv_output.get_dim(), dtype=torch.float16, device='cuda', layout=torch.strided).to(memory_format=torch.channels_last)
+
+    graph.execute({X: X_gpu, W: W_gpu, Y: Y_actual}, workspace)
+
+    # Compare
+    torch.testing.assert_close(Y_expected, Y_actual, atol=1e-2, rtol=1e-2)
+
+
+
+
+if __name__ == "__main__":
+    test_conv_relu()
+    test_gemm_relu_explicit([16, 32, 64, 128], [16,32,64,])
+    
