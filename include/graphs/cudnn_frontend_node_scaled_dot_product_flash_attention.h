58a75f4f63875c414cd090da30c72e4cd6f964b4
Anne Ouyang <aouyang@ipp2-1950.nvidia.com>
2023-07-31 11:05:23 -0700

    Fix elif -> else bug

--- include/graphs/cudnn_frontend_node_scaled_dot_product_flash_attention.h
+++ include/graphs/cudnn_frontend_node_scaled_dot_product_flash_attention.h
@@ -237,7 +237,7 @@ namespace cudnn_frontend::graph {
                     // Hard code data type input type as FE itself will place value in variant pack later
                     #if CUDNN_VERSION < 8930
                     .set_data_type(options.inputs.Q->get_data_type());
-                    #elif
+                    #else
                     .set_data_type(DataType_t::FLOAT);
                     #endif
 
@@ -276,7 +276,7 @@ namespace cudnn_frontend::graph {
             if(options.dropout_probability.has_value()) {
                 #if CUDNN_VERSION < 8930
                 half dropout_scale_value = (1.f / (1 - options.dropout_probability.value()));
-                #elif
+                #else
                 float dropout_scale_value = (1.f / (1 - options.dropout_probability.value()));
                 #endif
                 tensor_to_pass_by_value.emplace(dropout_scale, dropout_scale_value);
