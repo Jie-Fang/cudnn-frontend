6118ba456635d6124eaec3d5155ac3612a7452e8
Anerudhan Gopal <agopal@nvidia.com>
2023-11-01 22:14:56 -0700

    Check against boolean attention mask

--- include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
+++ include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
@@ -109,6 +109,13 @@ class ScaledDotProductFlashAttentionNode : public INode {
         RETURN_CUDNN_FRONTEND_ERROR_IF((((hidden_dim <= 128) && (hidden_dim % 8 == 0)) == false), error_code_t::GRAPH_NOT_SUPPORTED,
                                     "Num hidden_dim shoud be less than 128 and hidden_dim should be multiple of 8");
 
+        auto attn_mask =  attributes.inputs.find(input_names::Bias);
+        if (attn_mask != attributes.inputs.end() && attn_mask->second != nullptr) {
+            auto attn_mask_dtype = attn_mask->second->get_data_type();
+            RETURN_CUDNN_FRONTEND_ERROR_IF((attn_mask_dtype == DataType_t::BOOLEAN), error_code_t::GRAPH_NOT_SUPPORTED,
+                                    "Attn mask data type cannot be boolean");
+        }
+
         return {error_code_t::OK, ""};
     }
 
@@ -587,6 +594,13 @@ class ScaledDotProductFlashAttentionBackwardNode : public INode {
         RETURN_CUDNN_FRONTEND_ERROR_IF((((hidden_dim <= 128) && (hidden_dim % 8 == 0)) == false), error_code_t::GRAPH_NOT_SUPPORTED,
                                     "Num hidden_dim shoud be less than 128 and hidden_dim should be multiple of 8");
         
+        auto attn_mask =  attributes.inputs.find(input_names::Bias);
+        if (attn_mask != attributes.inputs.end() && attn_mask->second != nullptr) {
+            auto attn_mask_dtype = attn_mask->second->get_data_type();
+            RETURN_CUDNN_FRONTEND_ERROR_IF((attn_mask_dtype == DataType_t::BOOLEAN), error_code_t::GRAPH_NOT_SUPPORTED,
+                                    "Attn mask data type cannot be boolean");
+        }
+
         return {error_code_t::OK, ""};
     }
 
