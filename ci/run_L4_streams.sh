c4a31619a35f7f6f3162ed53a007831a2b0c8d8b
Maximilien Breughe <mbreughe@nvidia.com>
2023-11-09 14:26:59 -0800

    Set stream size to 16

--- ci/run_L4_streams.sh
+++ ci/run_L4_streams.sh
@@ -2,7 +2,7 @@
 
 input_file="./ci/L4_testlist.txt"
 total_lines=$(wc -l < "$input_file")
-stream_group_size=8
+stream_group_size=16
 
 for ((i = 1; i <= total_lines; i += stream_group_size)); do
     ./test/pycudnnTest.py -RgrStream --stream_start "$i" --stream_group_size "$stream_group_size" < "$input_file"
