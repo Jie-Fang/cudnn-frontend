3e41d287ef43ac7a45acb2b1da28a1be30e5e9e0
Maximilien Breughe <mbreughe@nvidia.com>
2023-11-22 13:16:07 -0800

    Removed epxlict_test.py
    This didn't add any real value, and isn't more than a regular sample.
    To avoid maintaining it, I removed it, and we'll keep using pycudnnTest based tests going forward

--- ci/run_pycudnntest.sh
+++ ci/run_pycudnntest.sh
@@ -20,8 +20,6 @@ function run_python_tests() {
     python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -1 -jsonTestName= ConvReductionCol2D_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef 
     # Legacy graphRunner test with timing loop
     python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -1 -jsonTestName= ConvReductionCol2D_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef -T10
-    # Explicit tests (not using test_graph)
-    pytest test/explicit_test.py
 }
 
 display_header
