d81817554a7b9c79630f784d157a5f0674806221
Maximilien Breughe <mbreughe@nvidia.com>
2023-11-27 15:06:49 -0800

    Removed backend on new -T10 test so we can have older versions of cudnn pass as well

--- ci/run_pycudnntest.sh
+++ ci/run_pycudnntest.sh
@@ -17,9 +17,9 @@ function run_python_tests() {
     # Python defined graph tests
     python test/pycudnnTest.py --testPath test/python_graph_defs/basic_tests.py
     # Legacy graphRunner test with reference
-    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -1 -jsonTestName= ConvAdd_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef 
+    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -jsonTestName= ConvAdd_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef 
     # Legacy graphRunner test with timing loop
-    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -1 -jsonTestName= ConvAdd_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef -T10 
+    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -jsonTestName= ConvAdd_abstract -x  -dimA 8,64,64,64 -filtA 64,64,3,3 -padA 1,1 -convStrideA 1,1 -dilationA 1,1 -Pin s -Pcomp s -Pout s -rtol 5e-3 -atol 5e-3 -minDevVer 800 -formatAll 1 -S  -b  -serialization 0 -gpuRef -T10 
 }
 
 display_header
