ce582992e55e099fad2ebaea6b4f74710e698f9e
Maximilien Breughe <mbreughe@nvidia.com>
2023-11-27 20:27:22 -0800

    Replaced data type to half to make it work on hopper with v8.6

--- ci/run_pycudnntest.sh
+++ ci/run_pycudnntest.sh
@@ -17,9 +17,9 @@ function run_python_tests() {
     # Python defined graph tests
     python test/pycudnnTest.py --testPath test/python_graph_defs/basic_tests.py
     # Legacy graphRunner test with reference
-    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -3 -jsonTestName= ConvRelu_abstract1 -x  -Pcomp s -Pin s -Pout s -convStrideA 1,1 -dilationA 1,1 -dimA 4,32,32,32 -filtA 32,32,3,3 -dimOut 4,32,30,30 -formatAll 1 -padA 0,0 -atol 2.5e-03 -rtol 2.5e-03 -minDevVer 750 -S  -b  -serialization 0 -gpuRef 
+    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -3 -jsonTestName= ConvRelu_abstract1 -x  -Pcomp h -Pin h -Pout h -convStrideA 1,1 -dilationA 1,1 -dimA 4,32,32,32 -filtA 32,32,3,3 -dimOut 4,32,30,30 -formatAll 1 -padA 0,0 -atol 3.0e-03 -rtol 2.5 -minDevVer 750 -S  -b  -serialization 0 -gpuRef
     # Legacy graphRunner test with timing loop
-    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -3 -jsonTestName= ConvRelu_abstract1 -x  -Pcomp s -Pin s -Pout s -convStrideA 1,1 -dilationA 1,1 -dimA 4,32,32,32 -filtA 32,32,3,3 -dimOut 4,32,30,30 -formatAll 1 -padA 0,0 -atol 2.5e-03 -rtol 2.5e-03 -minDevVer 750 -S  -b  -serialization 0 -gpuRef -T10 
+    python test/pycudnnTest.py -Dforce_jit_dbg= 1 -R graphRunner -backendEngine -3 -jsonTestName= ConvRelu_abstract1 -x  -Pcomp h -Pin h -Pout h -convStrideA 1,1 -dilationA 1,1 -dimA 4,32,32,32 -filtA 32,32,3,3 -dimOut 4,32,30,30 -formatAll 1 -padA 0,0 -atol 3.0e-03 -rtol 2.5 -minDevVer 750 -S  -b  -serialization 0 -gpuRef -T10
 }
 
 display_header
