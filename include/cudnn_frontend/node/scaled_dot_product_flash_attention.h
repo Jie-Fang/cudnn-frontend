6610929b37bcb78b77d2fa90764941c7df033307
Vedaanta Agarwalla <vagarwalla@ipp2-1950.nvidia.com>
2023-12-15 07:04:42 -0800

    Leaves non-64 seq_kv to backend

--- include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
+++ include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
@@ -323,7 +323,7 @@ class SDPANode : public INode {
         }
 
         // 2. (bug in cudnn backend) no padding with max_seq_len%64!=0
-        if ((s_kv % 64 != 0) && (!(attributes.padding_mask))) {
+        if ((s_kv % 64 != 0) && (!(attributes.padding_mask)) && (cudnnGetVersion() < 9000)) {
             auto col_index_attributes =
                 Pointwise_attributes().set_name("gen_col_index").set_mode(PointwiseMode_t::GEN_INDEX).set_axis(3);
             auto col_index_output = pointwise(last_output, col_index_attributes);
