a5fd0e49f728cba78b08521718662fdf6494da8b
Anerudhan Gopal <agopal@nvidia.com>
2024-01-09 15:02:41 -0800

    Revert the matmuls.cpp change

--- samples/cpp/matmuls.cpp
+++ samples/cpp/matmuls.cpp
@@ -252,36 +252,33 @@ TEST_CASE("Bias + Matmul", "[matmul][graph]") {
                             .set_name("A")
                             .set_dim({b, m, k})
                             .set_stride({m * k, k, 1})
-                            .set_data_type(fe::DataType_t::INT8);
+                            .set_data_type(fe::DataType_t::BFLOAT16);
     auto A            = graph.tensor(A_attributes);
     auto B_attributes = fe::graph::Tensor_attributes()
                             .set_name("B")
                             .set_dim({b, k, n})
                             .set_stride({k * n, n, 1})
-                            .set_data_type(fe::DataType_t::INT8);
+                            .set_data_type(fe::DataType_t::BFLOAT16);
     auto B = graph.tensor(B_attributes);
 
     // Create Bias vector
     auto Bias_attributes =
         fe::graph::Tensor_attributes().set_name("Bias").set_dim({b, m, 1}).set_stride({m, 1, 1}).set_data_type(
-            fe::DataType_t::FLOAT);
+            fe::DataType_t::BFLOAT16);
     auto Bias = graph.tensor(Bias_attributes);
 
     // Add ADD operation
-
-    
-    auto matmul_attributes =
-        fe::graph::Matmul_attributes().set_name("GEMM").set_compute_data_type(fe::DataType_t::INT32);
-    auto C = graph.matmul(A, B, matmul_attributes);
-    C->set_data_type(fe::DataType_t::FLOAT);
-
     auto pw_0_attributes = fe::graph::Pointwise_attributes()
                                .set_name("pw0_Add")
                                .set_mode(fe::PointwiseMode_t::ADD)
                                .set_compute_data_type(fe::DataType_t::FLOAT);
-    auto A_after_pw_0 = graph.pointwise(C, Bias, pw_0_attributes);
-    A_after_pw_0->set_output(true).set_data_type(fe::DataType_t::FLOAT);
+    auto A_after_pw_0 = graph.pointwise(A, Bias, pw_0_attributes);
+    A_after_pw_0->set_data_type(fe::DataType_t::BFLOAT16);
 
+    auto matmul_attributes =
+        fe::graph::Matmul_attributes().set_name("GEMM").set_compute_data_type(fe::DataType_t::FLOAT);
+    auto C = graph.matmul(A_after_pw_0, B, matmul_attributes);
+    C->set_output(true).set_data_type(fe::DataType_t::FLOAT);
 
     REQUIRE(graph.validate().is_good());
 
