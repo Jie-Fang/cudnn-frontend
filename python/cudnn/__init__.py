aa79b19715eff5ab287c2fe8364a25f65f2130c3
Vedaanta Agarwalla <vagarwalla@ipp2-1950.nvidia.com>
2024-01-30 07:14:43 -0800

    Unrolls all symbols imported from compiled module

--- python/cudnn/__init__.py
+++ python/cudnn/__init__.py
@@ -1,3 +1,18 @@
-from .compiled_module import *
+from .compiled_module import (        
+    backend_version
+    , destroy_handle
+    , norm_forward_phase
+    , reduction_mode
+    , behavior_note
+    , create_handle
+    , get_stream
+    , numerical_note
+    , set_stream
+    , build_plan_policy
+    , data_type
+    , heur_mode
+    , pygraph
+    , tensor
+)
 
 __version__ = '1.1.0'
