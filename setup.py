c4cd5d12e94eb9bacf78364acee471035d8b5fa0
Vedaanta Agarwalla <vagarwalla@nvidia.com>
2024-03-05 15:17:59 -0800

    Adds a job for testing pip wheels in CI
    * Adds -y to apt install
    
    * Corrects pytohn isntall in jobs
    
    * Adds python versions to test wheels
    
    * Fixes cmake flags
    
    * Fixes pip install for directory
    
    * builds only one wheel
    
    * Adds a job for testing pip wheels in CI

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ class CMakeBuild(build_ext):
 
         # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
         cmake_args = [
-            f"VERBOSE=1",
             f"-DPython_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
             f"-DCUDNN_FRONTEND_BUILD_PYTHON_BINDINGS=ON",
