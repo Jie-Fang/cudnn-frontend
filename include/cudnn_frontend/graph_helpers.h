6c6ee54bddf390d7e1100fa95a092960daff93a5
Anerudhan Gopal <agopal@nvidia.com>
2024-05-28 17:20:28 -0700

    Initial tool for reproduce a failure.
    * Initial tool for reproduce a failure.

--- include/cudnn_frontend/graph_helpers.h
+++ include/cudnn_frontend/graph_helpers.h
@@ -101,10 +101,8 @@ typedef struct [[nodiscard]] error_object {
     do {                                                                                                          \
         if (auto cudnn_retval = x; cudnn_retval != CUDNN_STATUS_SUCCESS) {                                        \
             std::stringstream error_msg;                                                                          \
-            char last_error[1024];                                                                                \
-            detail::get_last_error_string(last_error, sizeof(last_error));                                        \
             error_msg << #x << " failed with code: " << detail::get_error_string(cudnn_retval)                    \
-                      << ", and message: " << last_error;                                                         \
+                      << ", and message: " << detail::get_last_error_string_();                                   \
             getLogger() << "[cudnn_frontend] ERROR: " << error_msg.str() << " at " << __FILE__ << ":" << __LINE__ \
                         << std::endl;                                                                             \
             return {error_code_t::CUDNN_BACKEND_API_FAILED, error_msg.str()};                                     \
