Jie-Fang/cudnn-frontend

commit 152a0a3b044ed7bb49bd5c87f3bc2009d91009e2

Refactor helpers.h utility in cpp samples, separate legacy cpp samples
* Run legacy test also

* Fix build issue

* Fix batchnorm hang

* Change permissions

* Revert "Move "test/python_fe" to "test/python""

This reverts commit 420d2a224a24c9098aa2348cf7d84b2243e903e5.

* Revert "Move "test/unit_tests" to "test/cpp""

This reverts commit 6f3b56ab391babbc9aef6e1d0e1339a880880267.

* Refactor helpers.h utility in cpp samples, separate legacy cpp samples

* Move "test/unit_tests" to "test/cpp"

* Move "test/python_fe" to "test/python"

samples/cpp/CMakeLists.txt
@@ -1,0 +1,73 @@
+# target sources
+add_executable(
+    samples
+
+    sdpa/fp16_fwd.cpp
+    sdpa/fp16_bwd.cpp
+    sdpa/fp16_cached.cpp
+    sdpa/fp16_benchmark.cpp
+    sdpa/fp16_fwd_with_custom_dropout.cpp
+    sdpa/fp8_fwd.cpp
+    sdpa/fp8_bwd.cpp
+
+    convolution/fprop.cpp
+    convolution/fp8_fprop.cpp
+    convolution/int8_fprop.cpp
+    convolution/dgrads.cpp
+    convolution/wgrads.cpp
+
+    matmul/matmuls.cpp
+    matmul/fp8_matmul.cpp
+    matmul/int8_matmul.cpp
+    matmul/mixed_matmul.cpp
+
+    norm/batchnorm.cpp
+    norm/layernorm.cpp
+    norm/rmsnorm.cpp
+
+    misc/serialization.cpp
+    misc/autotuning.cpp
+    misc/parallel_compilation.cpp
+    misc/pointwise.cpp
+    misc/resample.cpp
+    misc/slice.cpp
+    misc/sm_carveout.cpp
+)
+
+# target flags
+if(MSVC)
+    target_compile_options(
+        samples PRIVATE
+        /W4 /WX # warning level 3 and all warnings as errors
+        /wd4100 # allow unused parameters
+        /wd4458 # local hides class member (currently a problem for all inline setters)
+        /wd4505 # unreferenced function with internal linkage has been removed
+        /wd4101 /wd4189 # unreferenced local
+        /bigobj # increase number of sections in .Obj file
+    )
+else()
+    target_compile_options(
+        samples PRIVATE
+        -Wall
+        -Wextra
+        -Werror
+        -Wno-unused-function
+    )
+endif()
+
+# target links
+target_link_libraries(
+    samples PRIVATE
+    Threads::Threads
+    Catch2::Catch2WithMain
+    cudnn_frontend
+    _cudnn_frontend_pch
+    CUDNN::cudnn
+)
+
+# target cmake properties
+set_target_properties(
+    samples PROPERTIES
+    LINK_WHAT_YOU_USE TRUE
+    RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin
+)

samples/CMakeLists.txt
@@ -1,114 +1,27 @@
 cmake_minimum_required(VERSION 3.18)
 
-find_package(Catch2 QUIET)
-
 find_package(Threads)
 
+find_package(Catch2 QUIET)
 if(NOT Catch2_FOUND)
-    Include(FetchContent)
+    include(FetchContent)
 
     # Fetch and build catch2
     FetchContent_Declare(
-      Catch2
-      GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-      GIT_TAG        v3.3.2
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v3.3.2
     )
     FetchContent_MakeAvailable(Catch2)
 endif()
 
-# Find cudnn
 include(${PROJECT_SOURCE_DIR}/cmake/cuDNN.cmake)
-
-add_executable(
-    samples
-    
-    cpp/sdpa/fp16_fwd.cpp
-    cpp/sdpa/fp16_bwd.cpp
-    cpp/sdpa/fp16_cached.cpp
-    cpp/sdpa/fp16_benchmark.cpp
-    cpp/sdpa/fp16_fwd_with_custom_dropout.cpp
-    cpp/sdpa/fp16_fwd_with_paged_caches.cpp
-    cpp/sdpa/fp8_fwd.cpp
-    cpp/sdpa/fp8_bwd.cpp
-
-    cpp/convolution/fprop.cpp
-    cpp/convolution/fp8_fprop.cpp
-    cpp/convolution/int8_fprop.cpp
-    cpp/convolution/dgrads.cpp
-    cpp/convolution/wgrads.cpp
-    
-    cpp/matmul/matmuls.cpp
-    cpp/matmul/fp8_matmul.cpp
-    cpp/matmul/int8_matmul.cpp
-    cpp/matmul/mixed_matmul.cpp
-
-    cpp/norm/batchnorm.cpp
-    cpp/norm/layernorm.cpp
-    cpp/norm/rmsnorm.cpp
-
-    cpp/misc/serialization.cpp
-    cpp/misc/autotuning.cpp
-    cpp/misc/parallel_compilation.cpp
-    cpp/misc/pointwise.cpp
-    cpp/misc/resample.cpp
-    cpp/misc/slice.cpp
-    cpp/misc/sm_carveout.cpp
-
-    legacy_samples/conv_sample.cpp 
-    legacy_samples/test_list.cpp 
-    legacy_samples/fp16_emu.cpp 
-    legacy_samples/helpers.cpp 
-    legacy_samples/fusion_sample.cpp 
-    legacy_samples/fp8_sample.cpp 
-    legacy_samples/norm_samples.cpp
-    legacy_samples/fused_mha_sample.cpp
-    legacy_samples/f16_flash_mha_sample.cpp
-    legacy_samples/fp8_flash_mha_sample.cpp
-)
 
 if(DEFINED ENV{NO_DEFAULT_IN_SWITCH})
     message("Default case in the switch is disabled")
     add_compile_definitions(NO_DEFAULT_IN_SWITCH)
 endif()
 
-if (MSVC)
-    target_compile_options(
-        samples PRIVATE
-        /W4 /WX # warning level 3 and all warnings as errors
-        /wd4100 # allow unused parameters
-        /wd4458 # local hides class member (currently a problem for all inline setters)
-        /wd4505 # unreferenced function with internal linkage has been removed
-        /wd4101 /wd4189 # unreferenced local
-        /bigobj # increase number of sections in .Obj file
-    )
-else()
-    target_compile_options(
-        samples PRIVATE
-        -Wall
-        -Wextra
-        -Werror
-        -Wno-unused-function
-    )
-endif()
-
-target_link_libraries(
-    samples
-
-    PRIVATE Threads::Threads
-
-    cudnn_frontend
-    _cudnn_frontend_pch
-    Catch2::Catch2WithMain
-
-
-    CUDNN::cudnn
-)
-
-# cuDNN dlopen's its libraries
-# Add all libraries in link line as NEEDED
-set_target_properties(
-    samples
-    PROPERTIES
-    LINK_WHAT_YOU_USE TRUE
-    RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin
-)
+# Add subdirectories for samples and legacy_samples
+add_subdirectory(cpp)
+add_subdirectory(legacy_samples)

samples/legacy_samples/CMakeLists.txt
@@ -1,0 +1,53 @@
+# target sources
+add_executable(
+    legacy_samples
+
+    conv_sample.cpp
+    test_list.cpp
+    fp16_emu.cpp
+    helpers.cpp
+    fusion_sample.cpp
+    fp8_sample.cpp
+    norm_samples.cpp
+    fused_mha_sample.cpp
+    f16_flash_mha_sample.cpp
+    fp8_flash_mha_sample.cpp
+)
+
+# target flags
+if(MSVC)
+    target_compile_options(
+        legacy_samples PRIVATE
+        /W4 /WX # warning level 3 and all warnings as errors
+        /wd4100 # allow unused parameters
+        /wd4458 # local hides class member (currently a problem for all inline setters)
+        /wd4505 # unreferenced function with internal linkage has been removed
+        /wd4101 /wd4189 # unreferenced local
+        /bigobj # increase number of sections in .Obj file
+    )
+else()
+    target_compile_options(
+        legacy_samples PRIVATE
+        -Wall
+        -Wextra
+        -Werror
+        -Wno-unused-function
+    )
+endif()
+
+# target links
+target_link_libraries(
+    legacy_samples PRIVATE
+    Threads::Threads
+    Catch2::Catch2WithMain
+    cudnn_frontend
+    _cudnn_frontend_pch
+    CUDNN::cudnn
+)
+
+# target cmake properties
+set_target_properties(
+    legacy_samples PROPERTIES
+    LINK_WHAT_YOU_USE TRUE
+    RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin
+)

ci/build.sh
@@ -4,7 +4,7 @@ function build_commands() {
     mkdir build
     cd build
     cmake -DCUDNN_FRONTEND_BUILD_PYTHON_BINDINGS=ON ../
-    cmake --build . -j16 --verbose
+    cmake --build . -j16
 }
 
 build_commands

ci/run_cpp_samples.sh
@@ -10,6 +10,7 @@ function display_header() {
 function run_cpp_samples() {
     cd build
     bin/samples --reporter JUnit::out=result-junit.xml --reporter console::out=-::colour-mode=ansi
+    bin/legacy_samples --reporter JUnit::out=result-junit.xml --reporter console::out=-::colour-mode=ansi
 }
 
 display_header

samples/cpp/convolution/dgrads.cpp
@@ -21,7 +21,7 @@
  */
 
 #include <catch2/catch_test_macros.hpp>
-#include "../../utils/helpers.h"
+#include "../utils/helpers.h"
 
 #include <cudnn_frontend.h>
 
@@ -49,7 +49,7 @@ TEST_CASE("Convolution Dgrad", "[dgrad][graph]") {
     DX->set_dim({4, 32, 16, 16}).set_output(true);
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     REQUIRE(graph.validate().is_good());
 
@@ -105,7 +105,7 @@ TEST_CASE("Dgrad Drelu Graph", "[dgrad][graph]") {
     DX->set_output(true);
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     REQUIRE(graph.validate().is_good());
 
@@ -209,7 +209,7 @@ TEST_CASE("Dgrad Drelu DBNweight Graph", "[dgrad][graph]") {
     }
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     REQUIRE(graph.validate().is_good());
     REQUIRE(graph.build_operation_graph(handle).is_good());

samples/cpp/convolution/fp8_fprop.cpp
@@ -21,7 +21,7 @@
  */
 
 #include <catch2/catch_test_macros.hpp>
-#include "../../utils/helpers.h"
+#include "../utils/helpers.h"
 
 #include <cudnn_frontend.h>
 
@@ -96,7 +96,7 @@ TEST_CASE("Convolution fp8 precision", "[conv][graph]") {
     REQUIRE(graph->validate().is_good());
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     REQUIRE(graph->build_operation_graph(handle).is_good());
     REQUIRE(graph->create_execution_plans({fe::HeurMode_t::A}).is_good());
@@ -130,5 +130,5 @@ TEST_CASE("Convolution fp8 precision", "[conv][graph]") {
 
     std::cout << graph->print() << std::endl;
     REQUIRE(graph->execute(handle, variant_pack, workspace.devPtr).is_good());
-    checkCudnnErr(cudnnDestroy(handle));
+    CUDNN_CHECK(cudnnDestroy(handle));
 }

samples/cpp/convolution/fprop.cpp
@@ -21,7 +21,7 @@
  */
 
 #include <catch2/catch_test_macros.hpp>
-#include "../../utils/helpers.h"
+#include "../utils/helpers.h"
 
 #include <cudnn_frontend.h>
 
@@ -69,7 +69,7 @@ TEST_CASE("Convolution fprop", "[conv][graph][caching]") {
 
     cudnnHandle_t handle;
 
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto [graph, X, W, Y] = build_new_graph(handle);
 
@@ -203,7 +203,7 @@ TEST_CASE("Convolution fprop dynamic shape", "[conv][graph][dynamic_shape]") {
     };
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     for (int idx_shape = 0; idx_shape < conv_shapes_count; ++idx_shape) {
         auto [graph, X, W, Y] = build_new_graph(handle, idx_shape);
@@ -291,7 +291,7 @@ TEST_CASE("CSBR Graph", "[conv][graph][caching]") {
     };
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto [graph, X, W, B, S, Y] = lookup_cache_or_build_graph(handle);
 
@@ -433,7 +433,7 @@ TEST_CASE("CSBR Graph dynamic shape", "[conv][graph][dynamic_shape]") {
     };
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     for (int idx_shape = 0; idx_shape < conv_shapes_count; idx_shape++) {
         auto [graph, X, W, B, S, Y] = lookup_cache_or_build_graph(handle, idx_shape);
@@ -525,7 +525,7 @@ TEST_CASE("SBRCS", "[conv][genstats][graph]") {
         SKIP("SBRCS requires Ampere or Hopper");
     }
 
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto [graph, X, W, B, S, Y, SUM, SQ_SUM] = build_new_graph(handle);
 
@@ -635,7 +635,7 @@ TEST_CASE("CBR Graph NCHW", "[conv][graph][caching]") {
     };
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto [graph, X, W, Z, B, Y] = lookup_cache_or_build_graph(handle);
 
@@ -719,7 +719,7 @@ TEST_CASE("Convolution fprop large", "[conv][graph][caching]") {
 
     cudnnHandle_t handle;
 
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto [graph, X, W, Y] = build_new_graph(handle);
 

commit 97716f38bab0f0dff627e7f3b9652c3505fa09a5

Option to set max KV sequence length explicitly
Three changes:

* Add back the cpp sample for paged caches
* Rename the paged attention related options
* Add the option set max KV sequence length explicitly

Reason for the latter:

**TL;DR: The issue is that we are leaving a trap behind for developers of any feature that requires input tensors with s_kv as one of the dimensions.** 

We have all kind of ways to infer s_kv: from the bias, from the RNG/dropout, and if none of those are present, we fall back to an overestimate of S_KV. I am anticipating that as we add new features to sdpa, there will be more ways to infer s_kv. Not adding this to the list of ways to infer s_kv will result in conflicts with our fallback estimate.Sure enough, with Flex Attention coming from behind the corner, this confirmed my point: Flex attention could be using tensors that use s_kv as one of the dimensions. We don't want future devs to keep updating how we infer s_kv in the case of paged attention. And neither do we want our "overestimated s_kv" conflicts with the actual s_kv from new features.

Commit history:

* Addressing windows build issue

* Addressed suggestion Ane

* Added a warning message

* Added back paged caches CPP sample that was removed

Commit 152a0a3b044ed7bb49bd5c87f3bc2009d91009e2 accidentally removed
samples/cpp/sdpa/fp16_fwd_with_paged_caches.cpp from being compiled.

This commit adds it back the the CMakeLists and adds the necessary
adjustments.

* Renamed paged attention vars and fixed small issues

* Typo

* Option to set max KV sequence length explicitly

include/cudnn_frontend/graph_properties.h
@@ -1415,6 +1415,7 @@ class SDPA_attributes : public Attributes<SDPA_attributes> {
     std::optional<int> sliding_window_length;
     std::optional<float> dropout_probability;
     std::optional<float> attn_scale_value;
+    std::optional<int> max_seq_len_kv;
 
    public:
     enum class input_names {
@@ -1539,14 +1540,20 @@ class SDPA_attributes : public Attributes<SDPA_attributes> {
     }
 
     SDPA_attributes&
-    set_page_table_K(std::shared_ptr<Tensor_attributes> value) {
+    set_paged_attention_k_table(std::shared_ptr<Tensor_attributes> value) {
         inputs[SDPA_attributes::input_names::Page_table_K] = value;
         return *this;
     }
 
     SDPA_attributes&
-    set_page_table_V(std::shared_ptr<Tensor_attributes> value) {
+    set_paged_attention_v_table(std::shared_ptr<Tensor_attributes> value) {
         inputs[SDPA_attributes::input_names::Page_table_V] = value;
+        return *this;
+    }
+
+    SDPA_attributes&
+    set_paged_attention_max_seq_len_kv(int const value) {
+        max_seq_len_kv = value;
         return *this;
     }
 };

include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
@@ -105,6 +105,11 @@ class SDPANode : public NodeCRTP<SDPANode> {
         bool const is_paged  = ((page_table_k_it) != attributes.inputs.end() &&  page_table_k_it->second != nullptr) ||  
                                ((page_table_v_it) != attributes.inputs.end() &&  page_table_v_it->second != nullptr);
 
+        auto const& rng_tensor = attributes.outputs.find(output_names::RNG_DUMP);
+        bool const is_rng   = (rng_tensor != attributes.outputs.end() && rng_tensor->second != nullptr);
+        
+        bool const max_seq_kv_explicit = attributes.max_seq_len_kv.has_value();
+
         // validation TODO:
         //    - validate stats has valid dims
 
@@ -189,6 +194,20 @@ class SDPANode : public NodeCRTP<SDPANode> {
             error_code_t::GRAPH_NOT_SUPPORTED,
             "Paged caches can only be used in combination with padding mask and variable sequence lengths for both Q and KV.");
 
+       RETURN_CUDNN_FRONTEND_ERROR_IF(!is_paged && max_seq_kv_explicit,
+            error_code_t::GRAPH_NOT_SUPPORTED, "When not using paged attention, there is no need to explicitly set max kv sequence length.");
+        
+        if (max_seq_kv_explicit){
+           auto max_seq_kv = attributes.max_seq_len_kv.value();
+           
+           RETURN_CUDNN_FRONTEND_ERROR_IF(is_bias && (bias_mask->second->get_dim()[3] != max_seq_kv),
+            error_code_t::GRAPH_NOT_SUPPORTED, "Value set through set_paged_attention_max_seq_len_kv is incompatible with the sequence length of the bias");
+
+           RETURN_CUDNN_FRONTEND_ERROR_IF(is_rng &&
+                    rng_tensor->second->get_dim()[3] != max_seq_kv,
+            error_code_t::GRAPH_NOT_SUPPORTED, "Value set through set_paged_attention_max_seq_len_kv is incompatible with the sequence length of the RNG_DUMP");
+        }
+
         // version specific validation
         RETURN_CUDNN_FRONTEND_ERROR_IF(detail::get_backend_version() < 8906 && ((s_kv % 64 != 0) || (d_qk % 64 != 0)),
                                        error_code_t::GRAPH_NOT_SUPPORTED,
@@ -269,35 +288,46 @@ class SDPANode : public NodeCRTP<SDPANode> {
 
         // Infer s_kv
         int64_t s_kv = -1;
+
+        // If s_kv was set explicitly, use that
+        if (attributes.max_seq_len_kv.has_value()) {
+            s_kv = attributes.max_seq_len_kv.value();
+        }
         // When one of K or V cache are paged, s_kv can be extracted directly
-        if (!is_paged_k) {
+        else if (!is_paged_k) {
             s_kv = k_dim[2];
 
         } else if (!is_paged_v) {
             s_kv = v_dim[2];
-            // If there is a bias, extract it from there
-        } else if (attributes.inputs[input_names::Bias] != nullptr) {
-            s_kv = attributes.inputs[input_names::Bias]->get_dim()[3];
-            // If there is an rng_dump output, extract it from there
-        } else if (attributes.outputs.find(output_names::RNG_DUMP) != attributes.outputs.end() &&
-                   attributes.outputs[output_names::RNG_DUMP] != nullptr) {
-            s_kv = attributes.outputs[output_names::RNG_DUMP]->get_dim()[3];
-            // When both caches are paged, and the above failed, we need to infer s_kv from the page table and
-            // container
         } else {
-            // [b, 1, ceil(s_kv/block_size), 1]
-            auto page_table_dim_k = attributes.inputs[input_names::Page_table_K]->get_dim();
-            // [b, h_k, block_size, d_k]
-            auto container_dim_k = attributes.inputs[input_names::K]->get_dim();
-            int64_t s_k          = page_table_dim_k[2] * container_dim_k[2];
+            CUDNN_FE_LOG_LABEL_ENDL(
+                "WARNING: maximum kv sequence length is being inferred. To set it explicitly, please use  "
+                "\"set_paged_attention_max_seq_len_kv\"");
 
-            // [b, 1, ceil(s_kv/block_size), 1]
-            auto page_table_dim_v = attributes.inputs[input_names::Page_table_V]->get_dim();
-            // [b, h_v, block_size, d_v]
-            auto container_dim_v = attributes.inputs[input_names::V]->get_dim();
-            int64_t s_v          = page_table_dim_v[2] * container_dim_v[2];
+            // If there is a bias, extract it from there
+            if (attributes.inputs[input_names::Bias] != nullptr) {
+                s_kv = attributes.inputs[input_names::Bias]->get_dim()[3];
+                // If there is an rng_dump output, extract it from there
+            } else if (attributes.outputs.find(output_names::RNG_DUMP) != attributes.outputs.end() &&
+                       attributes.outputs[output_names::RNG_DUMP] != nullptr) {
+                s_kv = attributes.outputs[output_names::RNG_DUMP]->get_dim()[3];
+                // When both caches are paged, and the above failed, we need to infer s_kv from the page table and
+                // container
+            } else {
+                // [b, 1, ceil(s_kv/block_size), 1]
+                auto page_table_dim_k = attributes.inputs[input_names::Page_table_K]->get_dim();
+                // [b, h_k, block_size, d_k]
+                auto container_dim_k = attributes.inputs[input_names::K]->get_dim();
+                int64_t s_k          = page_table_dim_k[2] * container_dim_k[2];
 
-            s_kv = std::min(s_k, s_v);
+                // [b, 1, ceil(s_kv/block_size), 1]
+                auto page_table_dim_v = attributes.inputs[input_names::Page_table_V]->get_dim();
+                // [b, h_v, block_size, d_v]
+                auto container_dim_v = attributes.inputs[input_names::V]->get_dim();
+                int64_t s_v          = page_table_dim_v[2] * container_dim_v[2];
+
+                s_kv = std::min(s_k, s_v);
+            }
         }
 
         std::shared_ptr<Tensor_attributes> k_cache;

python/pygraph/pygraph.h
@@ -285,8 +285,9 @@ class PyGraph {
          py::object const& sliding_window_length,
          py::object const& dropout,
          std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& rng_dump,
-         std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& page_table_k,
-         std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& page_table_v,
+         std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& paged_attention_k_table,
+         std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& paged_attention_v_table,
+         py::object const& paged_attention_max_seq_len_kv,
          cudnn_frontend::DataType_t const& compute_data_type,
          std::string const& name);
 

python/pygraph/sdpa.cpp
@@ -28,8 +28,9 @@ PyGraph::sdpa(std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& q,
               py::object const& sliding_window_length,
               py::object const& dropout,
               std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& rng_dump,
-              std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& page_table_k,
-              std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& page_table_v,
+              std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& paged_attention_k_table,
+              std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& paged_attention_v_table,
+              py::object const& paged_attention_max_seq_len_kv,
               cudnn_frontend::DataType_t const& compute_data_type,
               std::string const& name) {
     auto attributes = cudnn_frontend::graph::SDPA_attributes()
@@ -44,12 +45,20 @@ PyGraph::sdpa(std::shared_ptr<cudnn_frontend::graph::Tensor_attributes>& q,
                           .set_compute_data_type(compute_data_type)
                           .set_name(name);
 
-    if (page_table_k) {
-        attributes.set_page_table_K(page_table_k);
+    if (paged_attention_k_table) {
+        attributes.set_paged_attention_k_table(paged_attention_k_table);
     }
 
-    if (page_table_v) {
-        attributes.set_page_table_V(page_table_v);
+    if (paged_attention_v_table) {
+        attributes.set_paged_attention_v_table(paged_attention_v_table);
+    }
+
+    if (!paged_attention_max_seq_len_kv.is_none()) {
+        if (py::isinstance<py::int_>(paged_attention_max_seq_len_kv)) {
+            attributes.set_paged_attention_max_seq_len_kv(paged_attention_max_seq_len_kv.cast<int>());
+        } else {
+            throw std::runtime_error("paged_attention_max_seq_len_kv must be an integer.");
+        }
     }
 
     if (!attn_scale.is_none()) {
@@ -330,8 +339,9 @@ init_pygraph_sdpa_submodule(py::class_<PyGraph>& m) {
           py::arg_v("sliding_window_length", py::none()),
           py::arg_v("dropout", py::none()),
           py::arg_v("rng_dump", nullptr),
-          py::arg_v("page_table_k", nullptr),
-          py::arg_v("page_table_v", nullptr),
+          py::arg_v("paged_attention_k_table", py::none()),
+          py::arg_v("paged_attention_v_table", py::none()),
+          py::arg_v("paged_attention_max_seq_len_kv", py::none()),
           py::arg_v("compute_data_type", cudnn_frontend::DataType_t::NOT_SET),
           py::arg_v("name", ""),
           R"pbdoc(
@@ -353,8 +363,9 @@ init_pygraph_sdpa_submodule(py::class_<PyGraph>& m) {
                     sliding_window_length (Optional[int]): The length of sliding window. Default is None.
                     dropout (Optional[Union[Tuple[(probability: float, seed: cudnn_tensor, offset: cudnn_tensor)], Tuple[mask: cudnn_tensor, scale: cudnn_tensor]]]): Whether to do dropout. Default is None.
                     rng_dump (Optional[cudnn_tensor]): Debug tensor to dump the Philox RNG dropout mask. Default is None.
-                    page_table_k (Optional[cudnn_tensor]): The page table to look up offsets into 'k'
-                    page_table_v (Optional[cudnn_tensor]): The page table to look up offsets into 'v'
+                    paged_attention_k_table (Optional[cudnn_tensor]): The page table to look up offsets into 'k'
+                    paged_attention_v_table (Optional[cudnn_tensor]): The page table to look up offsets into 'v'
+                    paged_attention_max_seq_len_kv (Optional[integer]): The maximum sequence length for k/v caches when paged attention is active.
                     compute_data_type (Optional[cudnn.data_type]): The data type for computation. Default is NOT_SET.
                     name (Optional[str]): The name of the operation.
 

samples/cpp/CMakeLists.txt
@@ -7,6 +7,7 @@ add_executable(
     sdpa/fp16_cached.cpp
     sdpa/fp16_benchmark.cpp
     sdpa/fp16_fwd_with_custom_dropout.cpp
+    sdpa/fp16_fwd_with_paged_caches.cpp
     sdpa/fp8_fwd.cpp
     sdpa/fp8_bwd.cpp
 

samples/cpp/sdpa/fp16_fwd_with_paged_caches.cpp
@@ -21,7 +21,7 @@
  */
 
 #include <catch2/catch_test_macros.hpp>
-#include "../../utils/helpers.h"
+#include "../utils/helpers.h"
 
 #include <cuda_runtime_api.h>
 
@@ -135,8 +135,9 @@ create_sdpa_forward_graph_with_paged_caches(int64_t const b,
                                           .set_stride({{table_size, table_size, 1, 1}})
                                           .set_data_type(fe::DataType_t::INT32));
 
-    sdpa_options.set_page_table_K(page_table_k);
-    sdpa_options.set_page_table_V(page_table_v);
+    sdpa_options.set_paged_attention_k_table(page_table_k);
+    sdpa_options.set_paged_attention_v_table(page_table_v);
+    sdpa_options.set_paged_attention_max_seq_len_kv(static_cast<int>(s_kv));
 
     auto [O, Stats] = graph->sdpa(Q, K, V, sdpa_options);
 
@@ -176,7 +177,7 @@ TEST_CASE("Toy sdpa forward with paged caches", "[graph][sdpa][flash][paged][for
     }
 
     cudnnHandle_t handle;
-    checkCudnnErr(cudnnCreate(&handle));
+    CUDNN_CHECK(cudnnCreate(&handle));
 
     auto graph = create_sdpa_forward_graph_with_paged_caches(b,
                                                              h_q,
@@ -222,15 +223,15 @@ TEST_CASE("Toy sdpa forward with paged caches", "[graph][sdpa][flash][paged][for
         elem = distribution(rng);
     }
 
-    checkCudaErr(cudaMemcpy(page_table_k_tensor.devPtr,
-                            host_page_table_k.data(),
-                            sizeof(host_page_table_k[0]) * b,
-                            cudaMemcpyHostToDevice));
-    checkCudaErr(cudaMemcpy(page_table_v_tensor.devPtr,
-                            host_page_table_v.data(),
-                            sizeof(host_page_table_v[0]) * b,
-                            cudaMemcpyHostToDevice));
-    checkCudaErr(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaMemcpy(page_table_k_tensor.devPtr,
+                          host_page_table_k.data(),
+                          sizeof(host_page_table_k[0]) * b,
+                          cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(page_table_v_tensor.devPtr,
+                          host_page_table_v.data(),
+                          sizeof(host_page_table_v[0]) * b,
+                          cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     std::unordered_map<fe::graph::Tensor_attributes::uid_t, void*> variant_pack = {
         {Q_UID, q_tensor.devPtr},
@@ -251,13 +252,13 @@ TEST_CASE("Toy sdpa forward with paged caches", "[graph][sdpa][flash][paged][for
     std::vector<int32_t> hostActualSeqlenQ(b, 20);
     std::vector<int32_t> hostActualSeqlenKV(b, 20);
 
-    checkCudaErr(cudaMemcpy(
+    CUDA_CHECK(cudaMemcpy(
         devActualSeqlenQ.devPtr, hostActualSeqlenQ.data(), sizeof(hostActualSeqlenQ[0]) * b, cudaMemcpyHostToDevice));
-    checkCudaErr(cudaMemcpy(devActualSeqlenKV.devPtr,
-                            hostActualSeqlenKV.data(),
-                            sizeof(hostActualSeqlenKV[0]) * b,
-                            cudaMemcpyHostToDevice));
-    checkCudaErr(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaMemcpy(devActualSeqlenKV.devPtr,
+                          hostActualSeqlenKV.data(),
+                          sizeof(hostActualSeqlenKV[0]) * b,
+                          cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     variant_pack[SEQ_LEN_Q_UID]  = devActualSeqlenQ.devPtr;
     variant_pack[SEQ_LEN_KV_UID] = devActualSeqlenKV.devPtr;
@@ -273,7 +274,7 @@ TEST_CASE("Toy sdpa forward with paged caches", "[graph][sdpa][flash][paged][for
 
     REQUIRE(graph->execute(handle, variant_pack, workspace.devPtr).is_good());
 
-    checkCudaErr(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     cudnnDestroy(handle);
 }

test/python/test_mhas.py
@@ -724,8 +724,9 @@ def test_sdpa(
         sliding_window_length=sliding_window_length,
         dropout=dropout_tuple if is_dropout else None,
         rng_dump=rng_dump,
-        page_table_k=page_table_k,
-        page_table_v=page_table_v
+        paged_attention_k_table=page_table_k,
+        paged_attention_v_table=page_table_v,
+        paged_attention_max_seq_len_kv=s_kv if is_paged_attention else None
     )
 
     o.set_output(True).set_dim(shape_o).set_stride(stride_o)