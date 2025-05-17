cd00389859e965553d92a8e6d032087613bebda9
Anerudhan Gopal <agopal@nvidia.com>
2025-05-16 22:12:35 -0700

    Updating the CI for 9.10.1 builds
    * Updating the CI for 9.10.1 builds

--- dockers/pip_wheels/generate_image.sh
+++ dockers/pip_wheels/generate_image.sh
@@ -1,2 +1,2 @@
-docker image build --no-cache -t gitlab-master.nvidia.com:5005/cudnn/cudnn_frontend:pip_wheels_cuda_12.8_cudnn_9.8.0 -f dockers/pip_wheels/Dockerfile --build-arg CUDA_VERSION=12-8 --build-arg CUDNN_VERSION=9.8.0 .
-docker push gitlab-master.nvidia.com:5005/cudnn/cudnn_frontend:pip_wheels_cuda_12.8_cudnn_9.8.0
+docker image build --no-cache -t gitlab-master.nvidia.com:5005/cudnn/cudnn_frontend:pip_wheels_cuda_12.9_cudnn_9.10.1 -f dockers/pip_wheels/Dockerfile --build-arg CUDA_VERSION=12-9 --build-arg CUDNN_VERSION=9.10.1 .
+docker push gitlab-master.nvidia.com:5005/cudnn/cudnn_frontend:pip_wheels_cuda_12.9_cudnn_9.10.1
