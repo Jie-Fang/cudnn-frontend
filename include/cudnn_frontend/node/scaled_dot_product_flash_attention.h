6f69e4c3df6e4bb71eeb13cc5fb4033c37934d44
Vedaanta Agarwalla <vagarwalla@nvidia.com>
2025-12-17 13:43:56 -0800

    Allow ragged with deterministic bprop
    * Allow ragged with deterministic bprop

--- include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
+++ include/cudnn_frontend/node/scaled_dot_product_flash_attention.h
@@ -1008,9 +1008,9 @@ class CompositeSDPABackwardNode : public NodeCRTP<CompositeSDPABackwardNode> {
                                        "Dropout RNG dump is not supported for SM Major version 10");
 
         // TODO add version check once fixed
-        RETURN_CUDNN_FRONTEND_ERROR_IF(prop.major == 10 && is_ragged && (is_dbias || attributes.is_deterministic_algorithm),
+        RETURN_CUDNN_FRONTEND_ERROR_IF(prop.major == 10 && is_ragged && is_dbias,
                                        error_code_t::GRAPH_NOT_SUPPORTED,
-                                       "Deterministic kernel or dbias with ragged is not supported for SM Major version 10");
+                                       "dbias with ragged is not supported for SM Major version 10");
 
         // validate that datatype is set for the graph
         RETURN_CUDNN_FRONTEND_ERROR_IF(this->context.get_intermediate_data_type() == DataType_t::NOT_SET,
