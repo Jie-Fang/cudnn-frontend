104eb575b79994f9b3f50034179915348ef6e683
Vedaanta Agarwalla <vagarwalla@nvidia.com>
2026-02-16 19:58:13 -0800

    Fixes hardcoded heur for deterministic bprop
    * Fixes hardcoded heur for deterministic bprop
    
    * Fixes hardcoded heur for deterministic bprop

--- include/cudnn_frontend/node/sdpa_fp8_bwd.h
+++ include/cudnn_frontend/node/sdpa_fp8_bwd.h
@@ -632,7 +632,11 @@ class SDPAFP8BackwardNode : public NodeCRTP<SDPAFP8BackwardNode> {
     std::pair<int64_t, std::unordered_map<KnobType_t, int64_t>>
     override_heuristics_query() const {
         if (is_deterministic_algorithm_supported_on_blackwell) {
-            return {5, {{KnobType_t::KERNEL_CFG, 31}, {KnobType_t::STAGES, 2}}};
+            if(detail::get_backend_version() < 92100) {
+                return {5, {{KnobType_t::KERNEL_CFG, 31}, {KnobType_t::STAGES, 2}}};
+            } else {
+                return {5, {{KnobType_t::KERNEL_CFG, 1}, {KnobType_t::STAGES, 2}}};
+            }
         } else {
             return {-1, {}};
         }
