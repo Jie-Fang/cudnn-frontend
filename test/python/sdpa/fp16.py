8858acf7d1502557b1d1764f9023c3557f560463
Emil Gilliam <egilliam@nvidia.com>
2026-03-18 12:47:51 -0700

    Remove stride_stats_workaround
    * Simplify to just stride_stats
    
    * Fix for stride_stats_workaround

--- test/python/sdpa/fp16.py
+++ test/python/sdpa/fp16.py
@@ -132,7 +132,7 @@ def allocate_tensors(cfg, rng_data_gen, perf=False):
         allocs[TensorUid.k] = alloc_tensor(cfg.shape_k, cfg.data_type, strides=cfg.stride_k, rng=rng_data_gen, mean=-0.5, std=1.0, sparse_int=si)
         allocs[TensorUid.v] = alloc_tensor(cfg.shape_v, cfg.data_type, strides=cfg.stride_v, rng=rng_data_gen, mean=-0.5, std=1.0, sparse_int=si)
         allocs[TensorUid.o] = alloc_tensor(cfg.shape_o, cfg.data_type, strides=cfg.stride_o)
-        allocs[TensorUid.stats] = alloc_tensor(cfg.shape_stats, torch.float32, strides=cfg.stride_stats_workaround) if cfg.is_train else (None, None, None)
+        allocs[TensorUid.stats] = alloc_tensor(cfg.shape_stats, torch.float32, strides=cfg.stride_stats) if cfg.is_train else (None, None, None)
         allocs[TensorUid.score_max] = alloc_tensor(cfg.shape_stats, torch.float32, strides=cfg.stride_stats) if cfg.with_score_max else (None, None, None)
         allocs[TensorUid.score_sum_exp] = alloc_tensor(cfg.shape_stats, torch.float32, strides=cfg.stride_stats) if cfg.with_score_sum_exp else (None, None, None)
         if cfg.is_train:
@@ -286,7 +286,7 @@ def create_forward_graph(cfg, tensors, cudnn_handle):
             score_sum_exp.set_ragged_offset(stats_ragged_offset)
     
     if cfg.is_train:
-        stats.set_uid(int(TensorUid.stats)).set_output(True).set_data_type(cudnn.data_type.FLOAT).set_dim(cfg.shape_stats).set_stride(cfg.stride_stats_workaround)
+        stats.set_uid(int(TensorUid.stats)).set_output(True).set_data_type(cudnn.data_type.FLOAT).set_dim(cfg.shape_stats).set_stride(cfg.stride_stats)
         if cfg.is_ragged:
             stats.set_ragged_offset(stats_ragged_offset)
 
@@ -351,7 +351,7 @@ def create_backward_graph(cfg, tensors, cudnn_handle, max_t_q, max_t_kv):
     v = graph.tensor(uid=int(TensorUid.v), dim=cfg.shape_v, stride=cfg.stride_v, data_type=cudnn_dtype)
     o = graph.tensor(uid=int(TensorUid.o), dim=cfg.shape_o, stride=cfg.stride_o, data_type=cudnn_dtype)
     dO = graph.tensor(uid=int(TensorUid.dO), dim=cfg.shape_o, stride=cfg.stride_o, data_type=cudnn_dtype)
-    stats = graph.tensor(uid=int(TensorUid.stats), dim=cfg.shape_stats, stride=cfg.stride_stats_workaround, data_type=cudnn.data_type.FLOAT)
+    stats = graph.tensor(uid=int(TensorUid.stats), dim=cfg.shape_stats, stride=cfg.stride_stats, data_type=cudnn.data_type.FLOAT)
 
     bias_dim = (1, cfg.h_q, cfg.s_q, cfg.s_kv)
     bias_stride = (cfg.h_q * cfg.s_q * cfg.s_kv, cfg.s_q * cfg.s_kv, cfg.s_kv, 1)
