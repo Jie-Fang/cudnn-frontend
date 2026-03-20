9ebfc24585ae1ed6e93bde255654e24b6c5031de
Hwanseo Choi <hwanseoc@nvidia.com>
2026-03-19 17:20:40 -0700

    Add --apply mode to style check script
    * add apply mode to style check script

--- ci/run_style_check_diff.sh
+++ ci/run_style_check_diff.sh
@@ -2,6 +2,13 @@
 set -e
 clang-format --version
 black --version
-find include/ samples/ python/ -regex '.*\.\(cpp\|h\)$' | xargs clang-format --dry-run -Werror
-find test/ python/ -regex '.*\.py$' | xargs black --check --line-length 160
-find ./ -maxdepth 1 -regex '.*\.py$' | xargs black --check --line-length 160
+
+if [[ "$1" == "--apply" ]]; then
+    find include/ samples/ python/ -regex '.*\.\(cpp\|h\)$' | xargs clang-format -i
+    find test/ python/ -regex '.*\.py$' | xargs black --line-length 160
+    find ./ -maxdepth 1 -regex '.*\.py$' | xargs black --line-length 160
+else
+    find include/ samples/ python/ -regex '.*\.\(cpp\|h\)$' | xargs clang-format --dry-run -Werror
+    find test/ python/ -regex '.*\.py$' | xargs black --check --line-length 160
+    find ./ -maxdepth 1 -regex '.*\.py$' | xargs black --check --line-length 160
+fi
