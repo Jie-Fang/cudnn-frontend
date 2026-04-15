df3c122db49cf86c307dba8a5932d6cc3d8a71a8
Hwanseo Choi <hwanseoc@nvidia.com>
2026-04-15 09:04:21 -0700

    Add free-threading python 3.14t wheel builds
    * [ci-skip] Add Python 3.14t wheel build targets

--- ci/build_pip_wheels.sh
+++ ci/build_pip_wheels.sh
@@ -4,7 +4,7 @@ set -e
 
 DATE_FOLDER=`echo $(date '+%Y-%m-%d')`
 
-for version in cp314 cp313 cp312 cp311 cp310 cp39
+for version in cp314t cp314 cp313 cp312 cp311 cp310 cp39
 do
     echo "Building for ${version}"
     /opt/python/${version}-${version}/bin/python -m venv ${version}_env
